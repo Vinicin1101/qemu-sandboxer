dbf62f189c1fc4687b841ca4a9252fa5e2ee6ec8
Vinicin1101 <vserafim_o@outlook.com>
2026-05-19 15:43:58 -0300

    refactor(init): modularize lab initialization using TOML config
    - Replace hardcoded values with config values.
    - Fix Filesystem resize using virt-resize.

--- init.sh
+++ init.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-IMAGE_URL="https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-nocloud-amd64.qcow2"
-BASE_DIR="$(dirname "$0")/image"
-BASE_IMAGE="debian-base.qcow2"
 TEMP_SNAP="prep_snap.qcow2"
-SEED_ISO="seed.iso"
-GUEST_USER="cobaia"
-BASE_SHARED="/home/$GUEST_USER/code"
+
+if ! command -v python3 &> /dev/null; then
+    echo "[-] python3 is not installed. Please install it and try again."
+    exit 1
+fi
+eval "$(python3 parse_config.py)"
 
 echo "[*] Verifing base image"
-if [ ! -f "$BASE_DIR/$BASE_IMAGE" ]; then
-    echo "[+] Transfering image debian-nocloud..."
+if [ ! -f "$BASE_DIR/original_image.qcow2" ]; then
+    echo "[+] Transfering image..."
     curl -sL $IMAGE_URL -o $BASE_DIR/$BASE_IMAGE
+
+    cp "$BASE_DIR/$BASE_IMAGE" "$BASE_DIR/original_image.qcow2"
+else
+    echo -e "[+] Using existing image...\n If you want to start from scratch, delete '$BASE_DIR/original_image.qcow2' and run this script again."
+    cp "$BASE_DIR/original_image.qcow2" "$BASE_DIR/$BASE_IMAGE"
 fi
 
 echo "[+] Resizing image..."
-qemu-img resize "$BASE_DIR/$BASE_IMAGE" 16G
+mv "$BASE_DIR/$BASE_IMAGE" "$BASE_DIR/unsized_image.qcow2"
+qemu-img create -f qcow2 "$BASE_DIR/$BASE_IMAGE" 16G
+virt-resize --expand /dev/vda1 "$BASE_DIR/unsized_image.qcow2" "$BASE_DIR/$BASE_IMAGE"
+rm "$BASE_DIR/unsized_image.qcow2"
 
-echo "[+] Customizing image..."
+echo "[+] Customizing image $BASE_IMAGE..."
 virt-customize -a "$BASE_DIR/$BASE_IMAGE" \
-    --install ssh,curl,net-tools,vim \
-    --install build-essential,gdb,gcc-multilib,python3-pip,git \
+    --update \
+    --install $PACKAGES \
     --run-command "useradd -m -s /bin/bash $GUEST_USER" \
-    --password $GUEST_USER:password:$GUEST_USER \
+    --password "$GUEST_USER:password:$GUEST_PASS" \
     --run-command "usermod -aG sudo $GUEST_USER" \
-    --run-command "sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
+    --edit "/etc/ssh/sshd_config: s/#PasswordAuthentication yes/PasswordAuthentication yes/" \
     --run-command "systemctl enable ssh" \
     --edit '/etc/sudoers: s/^%sudo.*/%sudo ALL=(ALL) NOPASSWD:ALL/' \
-    --run-command "mkdir -p $BASE_SHARED" \
-    --run-command "chown $GUEST_USER:$GUEST_USER $BASE_SHARED" \
-    --append-line "/etc/fstab:host_share $BASE_SHARED 9p trans=virtio,version=9p2000.L,rw,_netdev 0 0"
+    --run-command "mkdir -p $SHARED_GUEST_DIR" \
+    --append-line "/etc/fstab:host_share $SHARED_GUEST_DIR 9p trans=virtio,version=9p2000.L,rw,_netdev 0 0" \
+    --edit "/etc/default/grub: s/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=0/" \
+    --run-command "update-grub"
 
-echo "[+] Success! Image has been customized and saved as '$BASE_IMAGE'."
-
-mkdir -p ./shared_code
+echo "[+] Success! Image has been customized and saved as $BASE_IMAGE."
